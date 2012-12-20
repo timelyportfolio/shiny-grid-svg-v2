db43d151c5429b0b09e9705f73ba2591659e5689
timelyportfolio <kent.russell@timelyportfolio.com>
2012-12-20 12:10:56 -0600

    first commit of original version
    will start here and then adapt to use htmlOutput

---
+++ ui.r
@@ -0,0 +1,24 @@
+reactiveSvg <- function (outputId) 
+{
+  HTML(paste("<div id=\"", outputId, "\" class=\"shiny-network-output\"><svg /></div>", sep=""))
+}
+
+shinyUI(
+  pageWithSidebar(
+    headerPanel("Shiny with gridSVG"),
+    sidebarPanel(
+      HTML("Motivated by the very fine work of Paul Murrell in this 
+            <a href=\"http://www.stat.auckland.ac.nz/~paul/Reports/ggplotSlider/ggplotSlider.html\">Technical Report</a>
+            and <a href=\"http://journal.r-project.org/current.html\">articles recently published in the R-Journal</a> I will show how to send
+            grid graphics as SVG to the browser with Shiny.<br></br>"),
+      selectInput("example", "Choose an example lattice chart (from lattice package documentation):", 
+                  choices = c("xyplot", "dotplot", "barchart")),
+      #submitButton("Make Grid SVG"),
+      helpText(HTML("All source available on <a href = \"https://github.com/timelyportfolio/shiny-grid-svg\">Github</a>"))
+      ),
+    mainPanel(
+      includeHTML("svgfiller.js"),
+      reactiveSvg(outputId = "svg.grid")
+      )
+    )
+  )
